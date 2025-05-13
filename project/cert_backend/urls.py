47ae51ae64b3b18586ddcb1096e022b50a433a9f
Praveenvin <pravinwin44@gmail.com>
2025-05-13 09:15:04 +0530

    added some extra files

--- project/cert_backend/urls.py
+++ project/cert_backend/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("certificates.urls")),  # NOT cert_backend.certificates
+    path('api/auth/', include('authentication.urls')),
+    path('api/certificates/', include('certificates.urls')),
+    path('api/students/', include('students.urls')),
 ]
